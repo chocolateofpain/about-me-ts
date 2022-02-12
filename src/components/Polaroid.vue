a4cd86dce91743082bd57313b15d9ca6bb9117d5
Elisabeth V <76956254+chocolateofpain@users.noreply.github.com>
2022-02-12 11:28:54 +0100

    feat: add routing to polaroids

--- src/components/Polaroid.vue
+++ src/components/Polaroid.vue
@@ -2,10 +2,12 @@
   <div
     class="border hover:shadow-2xl hover:underline p-2 shadow-lg w-[250px] h-[300px]"
   >
-    <img :src="getImageUrl()" v-bind:alt="image" width="250" height="250" />
-    <div class="mt-4">
-      {{ myTitle }}
-    </div>
+    <router-link :to="routerLink">
+      <img :src="getImageUrl()" v-bind:alt="image" width="250" height="250" />
+      <div class="mt-4">
+        {{ polaroidTitle }}
+      </div>
+    </router-link>
   </div>
 </template>
 <script lang="ts">
@@ -18,17 +20,20 @@ export default defineComponent({
     image: String,
   },
   setup(props) {
-    const myTitle = computed(() => props.title || "Hello Murder Raccoon");
+    const polaroidTitle = computed(() => props.title || "Hello Murder Raccoon");
 
     const getImageUrl = () => {
       return require("../assets/" + props.image);
     };
 
+    const routerLink = computed(() => `/${props.title?.toLowerCase()}`);
+
     onMounted(() => console.log("Your thing is mounted"));
 
     return {
-      myTitle,
+      polaroidTitle,
       getImageUrl,
+      routerLink,
     };
   },
 });
