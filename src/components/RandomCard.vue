63c9a33c6b269e3eb02526754cfda14e9a86e46a
Elisabeth V <76956254+chocolateofpain@users.noreply.github.com>
2022-05-05 08:29:41 +0200

    wip: add content to random view

---
+++ src/components/RandomCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ props.title }}</div>
+  <div>{{ props.description }}</div>
+  <div>{{ props.link || "no link yet" }}</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RandomCard",
+});
+</script>
--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="h-full bg-center bg-cover bg-pool sm:bg-none sm:bg-white">
     <Header class="hidden sm:block" title="Random" />
+    <RandomCard
+      v-for="item in randomItems"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :link="item.link"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/Header.vue";
+import RandomCard from "@/components/RandomCard.vue";
+import { randomItems } from "@/constants";
 
 export default defineComponent({
   name: "Random",
   components: {
     Header,
+    RandomCard,
+  },
+  setup() {
+    return {
+      randomItems,
+    };
   },
 });
 </script>
