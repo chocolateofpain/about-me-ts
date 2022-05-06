d9aadd3652e11c99e2adc6743b25922e26fdf4ca
Elisabeth V <76956254+chocolateofpain@users.noreply.github.com>
2022-05-06 08:25:40 +0200

    feat: add card component

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="h-full bg-center bg-cover bg-pool sm:bg-none sm:bg-white">
+  <div
+    class="justify-center h-full bg-center bg-cover bg-pool sm:bg-none sm:bg-white"
+  >
     <Header class="hidden sm:block" title="Random" />
-    <RandomCard
-      v-for="item in randomItems"
-      :key="item.id"
-      :title="item.title"
-      :description="item.description"
-      :link="item.link"
-    />
+    <div class="grid h-full grid-cols-1 place-items-center sm:grid-cols-2">
+      <RandomCard
+        v-for="item in randomItems"
+        :key="item.id"
+        :title="item.title"
+        :description="item.description"
+        :link="item.link"
+      />
+    </div>
   </div>
 </template>
 
