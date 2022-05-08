df42bb930b6472eb91b38bf9d8f730ca3ba91bbc
Elisabeth V <76956254+chocolateofpain@users.noreply.github.com>
2022-05-08 16:50:14 +0200

    feat: add ProjectCard

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <h1>{{ props.title || "Project name" }}</h1>
+    <p>{{ props.description || "what the project is about" }}</p>
+    <a :href="props.link">Website</a>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectCard",
+  setup(props) {
+    return {
+      props,
+    };
+  },
+});
+</script>
